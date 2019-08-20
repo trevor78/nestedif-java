c26d88873467fe70fd17b31d35a0b83e61df3e55
trevor78 <36665098+trevor78@users.noreply.github.com>
2019-08-20 15:33:04 +0530

    Update numberz.java

--- src/main/numberz.java
+++ src/main/numberz.java
@@ -8,7 +8,7 @@ class Number {
             if (n1 >= n3) {
                 largestNumber = n1;
                 if (n1 % 2 == 0){
-                    sum = n1+n2+n3+n4+n5+n6+n7+n8+n9+n10+n11+n12+n13+n14+n15+n16+n17+n18+n19+n20+n21+n22+n23+n24+n25
+                    sum = n1+n2+n3+n4+n5+n6+n7+n8+n9+n10+n11+n12+n13+n14+n15+n16+n17+n18+n19+n20
                     System.out.println("n1 is even");    
                 } else {
                     System.out.println("n1 is odd");
